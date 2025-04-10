2ff09da87e6c6065484f525a4287198a95a4b60c
rhythm-ravi <115625151+rhythm-ravi@users.noreply.github.com>
2025-04-10 13:32:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ operations = {
 }
 
 # Streamlit UI
-st.title("🧪 Image Processing Tool\n Rhythm Ravi")
+st.title(f"🧪 Image Processing Tool\n - Rhythm Ravi")
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png", "bmp", "tiff"])
 
